1932109a16bb56a85cb590460809afc4d7ca6e5c
williemckay <50088216+williemckay@users.noreply.github.com>
2026-08-19 16:46:42 +1200

    Updated bat file to match location on server

--- weeds_agol_processes.bat
+++ weeds_agol_processes.bat
@@ -1,8 +1,8 @@
 @ echo off
-echo: >> C:\Scripts\weedsProcessing\logWeedsProcessing.log
-echo: >> C:\Scripts\weedsProcessing\logWeedsProcessing.log
-echo Process Started on %date% at %time% >> C:\Scripts\weedsProcessing\logWeedsProcessing.log
+echo: >> C:\Scripts\weeds-processing\logWeedsProcessing.log
+echo: >> C:\Scripts\weeds-processing\logWeedsProcessing.log
+echo Process Started on %date% at %time% >> C:\Scripts\weeds-processing\logWeedsProcessing.log
 
-echo Starting Weeds Processing at %date% %time% >> C:\Scripts\weedsProcessing\logWeedsProcessing.log
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Scripts\weedsProcessing\agol_processes.py" >> C:\Scripts\weedsProcessing\logWeedsProcessing.log
-echo Scripts complete at %date% %time% >> C:\Scripts\weedsProcessing\logWeedsProcessing.log
+echo Starting Weeds Processing at %date% %time% >> C:\Scripts\weeds-processing\logWeedsProcessing.log
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Scripts\weeds-processing\agol_processes.py" >> C:\Scripts\weeds-processing\logWeedsProcessing.log
+echo Scripts complete at %date% %time% >> C:\Scripts\weeds-processing\logWeedsProcessing.log
